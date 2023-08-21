51dd63126b6c810ad7571eeda61fdf943b613e4c
John <john@Johns-Virtual-Machine.local>
2023-08-22 01:36:45 +0300

    fix(setup): more setup script improvements

--- setup.sh
+++ setup.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 
-## Define variables
 CONFIG=$HOME/.config
 DOTFILES=$HOME/.dotfiles
 
-## Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --skip-chsh
-rm $HOME/.zshrc
+echo '---- installing homebrew ----' &&
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/opt/homebrew/bin/brew shellenv)" &&
 
-## Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '---- installing nerd-font ----' &&
+brew tap homebrew/cask-fonts &&
+brew install --cask font-jetbrains-mono-nerd-font &&
 
-## Add nerd fonts repo and install one of them
-brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono-nerd-font
+echo '---- installing packages ----' &&
+brew install exa bat ripgrep w3m pandoc tmux neovim tree-sitter luarocks starship rustup zsh-autosuggestions jq &&
+brew install --cask iterm2 alacritty &&
 
-## Install packages
-brew install exa bat ripgrep w3m pandoc tmux neovim tree-sitter luarocks starship rustup
-brew install --cask iterm2 alacritty
+echo '---- installing rust ----' &&
+rustup-init -y &&
 
-## Install Rust
-rustup-init -y
+echo '---- installing node vesrsion manager ----' &&
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$(curl -sL https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r ".tag_name")/install.sh | bash &&
+export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+nvm install --lts &&
 
-mkdir -p $CONFIG/git
+echo '---- applying configs ----' &&
+mkdir -p $CONFIG/git &&
+cp $DOTFILES/git/config $CONFIG/git/config &&
+ln -s $DOTFILES/git/.gitignore $CONFIG/git/.gitignore &&
+git config --global core.excludesfile $CONFIG/git/.gitignore &&
+ln -s $DOTFILES/.zshrc $HOME/.zshrc &&
+ln -s $DOTFILES/alacritty $CONFIG/alacritty &&
+ln -s $DOTFILES/nvim $CONFIG/nvim &&
+ln -s $DOTFILES/tmux $CONFIG/tmux &&
 
-cp $DOTFILES/git/config $CONFIG/git/config
-ln -s $DOTFILES/git/.gitignore $CONFIG/git/.gitignore
-git config --global core.excludesfile $CONFIG/git/.gitignore
+echo '---- installing ohmyzsh ----' &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc &&
+/bin/zsh ~/.zshrc
 
-ln -s $DOTFILES/.zshrc $HOME/.zshrc
-ln -s $DOTFILES/alacritty $CONFIG/alacritty
-ln -s $DOTFILES/nvim $CONFIG/nvim
-ln -s $DOTFILES/tmux $CONFIG/tmux
