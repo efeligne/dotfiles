c2c74582ba2c3f0c426f3378eb32f56a2f39bf5d
efeligne <efeligne@icloud.com>
2023-08-27 12:59:43 +0300

    feat(git): add usefull git pluigns

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,10 @@
+return {
+  'neogitorg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'sindrets/diffview.nvim',
+    'ibhagwan/fzf-lua',
+  },
+  config = true,
+}
